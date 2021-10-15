bb298aed094eed91892cc2b26bb1b91697f1596e
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-10-14 20:24:08 -0400

    Update Greet.java

--- test/Greet.java
+++ test/Greet.java
@@ -1,6 +1,8 @@
 public class Greet {
   public static void main(String[] args) {
     BigSib george = new BigSib();
+    BigSib emily = new BigSib("Shsheesh");
     System.out.println(george.greet("Jeff"));
+    System.out.println(emily.greet("Faley");
   }
 }
