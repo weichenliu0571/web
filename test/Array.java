1df27f33963cee34813a7a078147b2b12f5da6ea
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-11-04 18:58:20 -0400

    Update and rename BigSib.java to Array.java

---
+++ test/Array.java
@@ -0,0 +1,22 @@
+public class Array {
+	public static void main(String[] args) {
+		System.out.println(gcd(13,15));
+	}
+
+	public static int gcd(int a, int b) {
+		int counter = 0;
+		int ans = 0;
+		int smaller = a;
+		if ( b < a) {
+			smaller = b;
+		}
+
+		while (counter < smaller + 1) {
+			if (a % counter == 0 && b % counter == 0) {
+				ans = counter;
+			}
+		}
+		return ans;
+	}
+
+}
