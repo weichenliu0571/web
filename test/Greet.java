5e9f7e2d97a6805cf747976d4a34a7d0c0204010
Weichen <wliu30@stuy.edu>
2021-11-04 19:25:26 -0400

    new

--- test/Greet.java
+++ test/Greet.java
@@ -1,4 +1,8 @@
 public class Greet {
+  public static void main(String[] args) {
+    System.out.println(gcd(5,15));
+    System.out.println(gcd(5,11));
+  }
   public static int gcd(int a, int b) {
     while (a != b) {
       if (a > b) {
