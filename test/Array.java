fd24b33c8320cc6f7ced8f786a21ffd964aea749
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-11-04 19:04:41 -0400

    Update Array.java

--- test/Array.java
+++ test/Array.java
@@ -6,7 +6,7 @@ public class Array {
 
 	  public static int gcd(int a, int b){
       int smaller;
-      int counter = 1;
+      int counter = 0;
       int accumulatedGCD = 1;
       if (a > b){
         smaller = b;
