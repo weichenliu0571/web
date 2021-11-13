ec672ba3dd514e9947bfdd4261088ae1062ee385
Weichen <wliu30@stuy.edu>
2021-11-13 09:30:49 -0500

    'a

--- test/Greet.java
+++ test/Greet.java
@@ -1,13 +1,60 @@
-public class Greet {
-  public static void main(String[] args) {
-    int p = 3;
-    int q = 1;
-    int sum = 0;
-    while (p <= 10) {
-      sum += p % q;
-      p+=1;
-      q+=1;
+  import java.lang.Math;
+
+  public class Greet {
+
+    public static int[] arrayRandInts(int size) {
+      int[] randInts = new int[size];
+      for (int x = 0 ; x < size ; x += 1) {
+        randInts[x] = (int)(Math.random() * 10);
+      }
+      return randInts;
     }
-    System.out.println(sum);
-  }
+
+
+    public static String strArrayInts(int[] intArray) {
+      String out = ("{" + intArray[0]);
+      for (int x = 1 ; x < intArray.length ; x += 1) {
+        out += ", " + intArray[x];
+      }
+      out += "}";
+      return out;
+    }
+
+
+    public static int linSearch(int[] a, int target) {
+      for (int x = 0 ; x < a.length ; x += 1) {
+        if (a[x] == target) {
+          return x;
+        }
+      }
+      return -1;
+    }
+
+    //
+    // public static int linSearchR(int[] a, int target) {
+    //   int leng = a.length - 1
+    //   if (a[0] == target) {
+    //     return
+    //   }
+    // }
+
+    public static int freq( int[] a, int target ) {
+      int answer = 0;
+      int counter = 0;
+      while (counter < int.length - 1) {
+        if (a[counter] == target) {
+          answer ++
+        }
+        counter ++;
+      }
+      return answer;
+    }
+
+
+    public static void main(String[] args) {
+      int[] local = arrayRandInts(5);
+      System.out.println(strArrayInts(local));
+      System.out.println(linSearch(local, 5));
+      System.out.println(freq(local, 5));
+    }
 }
