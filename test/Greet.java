dacf95c967459ee0947aff8cc6bb4005866c498c
Weichen <wliu30@stuy.edu>
2021-11-13 09:32:03 -0500

    a

--- test/Greet.java
+++ test/Greet.java
@@ -43,7 +43,7 @@
       int counter = 0;
       while (counter < int.length - 1) {
         if (a[counter] == target) {
-          answer ++
+          answer ++;
         }
         counter ++;
       }
