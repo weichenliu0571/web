ce95a1e9e6b4266df73593a9f3c2c15f9044a46f
Weichen <wliu30@stuy.edu>
2021-12-22 21:31:38 -0500

    'a

--- test/Driver.java
+++ test/Driver.java
@@ -1,11 +1,11 @@
 public class Driver implements InterTrash{
-  public void sleep(String b ) {
+  public void sleep(int b ) {
     System.out.println(b);
   }
 
   public static void main(String[] args) {
     Driver jeff = new Driver();
-    jeff.sleep("5");
+    jeff.sleep(5);
   }
 
 }
