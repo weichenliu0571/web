4fb28f212a348bef026e56b8d21e2dedd9a78a38
Weichen <wliu30@stuy.edu>
2022-01-18 20:39:15 -0500

    a

--- test/InterTrash.java
+++ test/InterTrash.java
@@ -18,41 +18,49 @@ public class InterTrash {
         for (int j = 3; j < 6; j++) {
           result += board[i][j];
         }
+        result += "|";
       }
       for (int i = 0; i < 3; i++) {
         for (int j = 6; j < 9; j++) {
           result += board[i][j];
         }
+        result += "|";
       }
       for (int i = 3; i < 6; i++) {
         for (int j = 0; j < 3; j++) {
           result += board[i][j];
         }
+        result += "|";
       }
       for (int i = 3; i < 6; i++) {
         for (int j = 3; j < 6; j++) {
           result += board[i][j];
         }
+        result += "|";
       }
       for (int i = 3; i < 6; i++) {
         for (int j = 6; j < 9; j++) {
           result += board[i][j];
         }
+        result += "|";
       }
       for (int i = 6; i < 9; i++) {
         for (int j = 0; j < 3; j++) {
           result += board[i][j];
         }
+        result += "|";
       }
       for (int i = 6; i < 9; i++) {
         for (int j = 3; j < 6; j++) {
           result += board[i][j];
         }
+        result += "|";
       }
       for (int i = 6; i < 9; i++) {
         for (int j = 6; j < 9; j++) {
           result += board[i][j];
         }
+        result += "|";
       }
      System.out.println(result);
 
