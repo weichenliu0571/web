c7be1b3c5dcb6f3f518705c870fb57cca6cccc0b
Weichen <wliu30@stuy.edu>
2022-01-18 20:40:09 -0500

    a

--- test/InterTrash.java
+++ test/InterTrash.java
@@ -12,55 +12,55 @@ public class InterTrash {
         for (int j = 0; j < 3; j++) {
           result += board[i][j];
         }
-        result += "|";
+        result += "|\n";
       }
       for (int i = 0; i < 3; i++) {
         for (int j = 3; j < 6; j++) {
           result += board[i][j];
         }
-        result += "|";
+        result += "|\n";
       }
       for (int i = 0; i < 3; i++) {
         for (int j = 6; j < 9; j++) {
           result += board[i][j];
         }
-        result += "|";
+        result += "|\n";
       }
       for (int i = 3; i < 6; i++) {
         for (int j = 0; j < 3; j++) {
           result += board[i][j];
         }
-        result += "|";
+        result += "|\n";
       }
       for (int i = 3; i < 6; i++) {
         for (int j = 3; j < 6; j++) {
           result += board[i][j];
         }
-        result += "|";
+        result += "|\n";
       }
       for (int i = 3; i < 6; i++) {
         for (int j = 6; j < 9; j++) {
           result += board[i][j];
         }
-        result += "|";
+        result += "|\n";
       }
       for (int i = 6; i < 9; i++) {
         for (int j = 0; j < 3; j++) {
           result += board[i][j];
         }
-        result += "|";
+        result += "|\n";
       }
       for (int i = 6; i < 9; i++) {
         for (int j = 3; j < 6; j++) {
           result += board[i][j];
         }
-        result += "|";
+        result += "|\n";
       }
       for (int i = 6; i < 9; i++) {
         for (int j = 6; j < 9; j++) {
           result += board[i][j];
         }
-        result += "|";
+        result += "|\n";
       }
      System.out.println(result);
 
