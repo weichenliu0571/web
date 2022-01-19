6ff4f1e861bec6d334a2fa8325e98957331a3395
Weichen <wliu30@stuy.edu>
2022-01-18 20:31:46 -0500

    a

--- test/InterTrash.java
+++ test/InterTrash.java
@@ -1,4 +1,52 @@
-public interface InterTrash {
-  public void sleep(int b);
+public class InterTrash {
+
+  public static void main(String[] args) {
+    String result = "";
+      for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+          result += board[i][j];
+        }
+      }
+      for (int i = 0; i < 3; i++) {
+        for (int j = 3; j < 6; j++) {
+          result += board[i][j];
+        }
+      }
+      for (int i = 0; i < 3; i++) {
+        for (int j = 6; j < 9; j++) {
+          result += board[i][j];
+        }
+      }
+      for (int i = 3; i < 6; i++) {
+        for (j = 0; j < 3; j++) {
+          result += board[i][j];
+        }
+      }
+      for (int i = 3; i < 6; i++) {
+        for (int j = 3; j < 6; j++) {
+          result += board[i][j];
+        }
+      }
+      for (int i = 3; i < 6; i++) {
+        for (int j = 6; j < 9; j++) {
+          result += board[i][j];
+        }
+      }
+      for (int i = 6; i < 9; i++) {
+        for (int j = 0; j < 3; j++) {
+          result += board[i][j];
+        }
+      }
+      for (int i = 6; i < 9; i++) {
+        for (int j = 3; j < 6; j++) {
+          result += board[i][j];
+        }
+      }
+      for (int i = 6; i < 9; i++) {
+        for (int j = 6; j < 9; j++) {
+          result += board[i][j];
+        }
+      }
+     System.out.println(result);
 
 }
